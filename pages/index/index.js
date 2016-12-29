bd104e43e2fb0ce9909c0af7f887af7838248037
ChengGuan <chengguan@ChengGueMacBook.lan>
2016-12-29 23:10:44 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,7 +11,17 @@ Page({
     onLoad: function () {
         var that = this
         const user = AV.User.current();
-        that.setData({userInfo:user.toJSON(),category_array: app.category_array})
+        wx.getUserInfo({
+            success: ({userInfo}) => {
+                // 更新当前用户的信息
+                user.set(userInfo).save().then(user => {
+                    // 成功，此时可在控制台中看到更新后的用户信息
+                    app.globalData.user = user.toJSON();
+                    console.log(app.globalData.user)
+                }).then(that.setData({userInfo:user.toJSON(),category_array: app.category_array})
+).catch(console.error);
+            }
+        });
     },
     onShow:function () {
         var that = this;
